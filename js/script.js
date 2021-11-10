bf8c89b5f26aa10584d9713062f1aeae6caa15f7
flo_h <flo.1244@gmail.com>
2021-11-09 19:54:08 -0500

    Phase 2

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
 //Global variables
 const overview = document.querySelector(".overview");/*Selects profile information*/
 const username = "flo1244"; 
+const repoList = document.querySelector(".repo-list");/*Selects the ul*/
 
 //API connecting to github repos
 const getData = async function () {
@@ -28,4 +29,24 @@ const userInfo = function (data) {
       <p><strong>Number of public repos:</strong> ${data.public_repos}</p>
 	</div>`;
 	overview.append(div);
+	repoData();
+};
+
+//Fetch repos async function 
+const repoData = async function () {
+	const getRepos = await fetch(`https://api.github.com/users/${username}/repos?sort=update&per_page=100`);
+	const repoInfo = await getRepos.json();
+	
+	//console.log(repoInfo);
+	showRepos(repoInfo);
+};
+
+//Displays info about repos
+const showRepos = function (repos) {
+	for(const repo of repos){
+		const list = document.createElement("li"); /*creates list item*/
+		list.classList.add("repo");/*adds class repo to item*/
+		list.innerHTML = `<h3>${repo.name}</h3>`;/*display repo name*/
+		repoList.append(list);
+	}
 };
